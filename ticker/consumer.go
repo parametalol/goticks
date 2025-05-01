cf7a63cf180e9c4d859f72ef2f4ba2361a715ed9
Michaël Petrov <smartptr@gmail.com>
2025-05-01 22:14:53 +0200

    consumer nits

--- ticker/consumer.go
+++ ticker/consumer.go
@@ -2,34 +2,41 @@ package ticker
 
 import "iter"
 
+type tack[TickType any] struct {
+	tick  TickType
+	ackCh chan struct{}
+}
+
+// consumer wraps a tick channel and synchronously acknowledges the tick
+// processing.
 type consumer[TickType any] struct {
-	tickCh  chan TickType
-	ackCh   chan struct{}
+	tickCh  chan tack[TickType]
 	closeCh chan struct{}
 	doneCh  chan struct{}
 }
 
 func newConsumer[TickType any]() *consumer[TickType] {
 	return &consumer[TickType]{
-		make(chan TickType),
-		make(chan struct{}),
-		make(chan struct{}),
-		make(chan struct{}),
+		tickCh:  make(chan tack[TickType]),
+		closeCh: make(chan struct{}),
+		doneCh:  make(chan struct{}),
 	}
 }
 
 // send is the writer method that sends ticks to the consumer.
 func (c *consumer[TickType]) send(tick TickType) {
+	tack := tack[TickType]{tick, make(chan struct{})}
 	select {
 	case <-c.doneCh:
 	case <-c.closeCh:
 		close(c.tickCh)
-	case c.tickCh <- tick:
-		<-c.ackCh
+	case c.tickCh <- tack:
+		<-tack.ackCh
 	}
 }
 
 // close is the writer method that closes the consumer.
+// The closed consumer won't receive more ticks, and cannot be reopened.
 func (c *consumer[TickType]) close() {
 	close(c.closeCh)
 }
@@ -37,20 +44,19 @@ func (c *consumer[TickType]) close() {
 // ticks returns an iterator that consumes all ticks and notifies the writer
 // when the tick is processed.
 func (c *consumer[TickType]) ticks() iter.Seq[TickType] {
-	return func(f func(t TickType) bool) {
-		defer close(c.ackCh)
+	return func(yield func(t TickType) bool) {
 		defer close(c.doneCh)
 		for {
 			select {
-			case tick, ok := <-c.tickCh:
-				if ok {
-					ok = f(tick)
-					c.ackCh <- struct{}{}
-					if ok {
-						continue
-					}
+			case tickAck, ok := <-c.tickCh:
+				if !ok {
+					return
 				}
-				return
+				ok = yield(tickAck.tick)
+				close(tickAck.ackCh)
+				if !ok {
+					return
+				}
 			case <-c.closeCh:
 				return
 			}
